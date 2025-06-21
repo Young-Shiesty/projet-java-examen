a244cf4750d438ebdc7f1a5391056643bde9c22e
gaye_ <gaye_@DESKTOP-N5C7IED>
2025-06-21 14:55:24 +0100

    dashbord admin

--- src/dao/UtilisateurDao.java
+++ src/dao/UtilisateurDao.java
@@ -5,12 +5,14 @@ import models.Utilisateur;
 import java.sql.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class UtilisateurDao {
     private static final String sqlinsert = "INSERT INTO UTILISATEURS(nom,prenom,username,password) values (?,?,?,?)";
     private static final String SQL_SELECT = "SELECT * FROM  UTILISATEURS";
     private static final String SQL_Connexion = "SELECT * FROM  UTILISATEURS where username=? ";
+    
+
 
     public static Utilisateur createuser(Utilisateur user) throws SQLException, ClassNotFoundException { 
         long idinserted = DatabaseService.executeInsertWithGeneratedKey(sqlinsert, user.getNom(), user.getPrenom(), user.getUsername(), user.getPassword());
@@ -38,6 +40,16 @@ public class UtilisateurDao {
     public static boolean usernameExiste(String username) throws SQLException, ClassNotFoundException {
     Connection con = DatabaseService.getConnection();
     PreparedStatement pst = con.prepareStatement("SELECT * FROM utilisateurs WHERE username = ?");
+    pst.setString(1, username);
+    ResultSet rs = pst.executeQuery();
+    if (rs.next()) {
+        return true;
+    }
+    return false;
+}
+    public static boolean joueurExiste(String username) throws SQLException, ClassNotFoundException {
+    Connection con = DatabaseService.getConnection();
+    PreparedStatement pst = con.prepareStatement("SELECT * FROM joueurs WHERE username = ?");
     pst.setString(1, username);
     ResultSet rs = pst.executeQuery();
     if (rs.next()) {
