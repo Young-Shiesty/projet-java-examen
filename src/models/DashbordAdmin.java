3a45313e5045d48aa339f159335380080cdb74eb
Youngshiesty <mamzf18@gmail.com>
2025-06-21 15:01:18 +0100

    j'ai deplacer le dashboard admin

--- src/models/DashbordAdmin.java
+++ src/models/DashbordAdmin.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package formulaire;
+package models;
+
+import formulaire.CreerTournois;
+import formulaire.ListeJoueurs;
 
 /**
  *
